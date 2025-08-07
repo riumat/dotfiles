c09a44628ce5e93b960556372dd540a47899f7ac
riumat <matteoariu42@gmail.com>
2025-08-07 18:45:53 +0200

    Update VSCodium setup script and settings; add custom CSS import and remove redundant entry

--- myinit/arch/codium.sh
+++ myinit/arch/codium.sh
@@ -13,11 +13,11 @@ cp vsc/settings.json ~/.config/VSCodium/User/
 cp vsc/keybindings.json ~/.config/VSCodium/User/
 cp vsc/custom.css ~/.config/VSCodium/css/
 
+sed -i '1a\    "vscode_custom_css.imports": [\n        "file://'$HOME'/.config/VSCodium/css/custom.css"\n    ],' ~/.config/VSCodium/User/settings.json
+
 PRODUCT="/opt/vscodium/resources/app/product.json"
 if ! grep -q '"enableCustomizations": true' "$PRODUCT"; then
     sudo sed -i '1s|{|{"enableCustomizations": true,|' "$PRODUCT"
 fi
 
 codium --install-extension vsc/extensions/vscode-custom-css.vsix
-
-codium --user-data-dir ~/.config/VSCodium
